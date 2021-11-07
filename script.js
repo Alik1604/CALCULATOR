b5ee3d142581f438ece764bfbad7e70153a4c839
Albert <rudenko.albert.n@gmail.com>
2021-11-07 13:23:05 +0200

    some bags removed T_T

--- script.js
+++ script.js
@@ -2,23 +2,49 @@ const buttons = document.querySelectorAll(".num_button");
 const op_buttons = document.querySelectorAll(".op_button");
 const res = document.querySelector("#res");
 const res_button = document.getElementById("res_button");
+const dot = document.getElementById("dot");
+const clear_button = document.getElementById("cl_button");
 
 let res_text = '';
 let num1 = 0;
 let num2 = 0;
 let operator = '';
+let dot_switch = false;
 
 buttons.forEach( button => button.addEventListener('click',take_num));
 op_buttons.forEach( op_button => op_button.addEventListener('click',operate));
 res_button.addEventListener('click',calc);
+dot.addEventListener('click',add_dot);
+clear_button.addEventListener('click',clear);
 
-
-function operate(op_button) {
-   num1 = res.textContent;
-   operator = op_button.target.textContent;
-   console.log(operator)
+function clear() {
    res.textContent = '';
    res_text= '';
+   dot_switch = false;
+   operator = '';
+}
+
+function add_dot() {
+   if ( dot_switch != true){
+      res_text = res_text + dot.textContent;
+      res.textContent = res_text;
+      dot_switch = true;
+   }
+   
+}
+
+function operate(op_button) {
+   if (operator == '') {
+      num1 = res.textContent;
+      operator = op_button.target.textContent;
+      res.textContent = '';
+      res_text= '';
+      dot_switch = false;
+   } 
+   num2 = res.textContent;
+   if ( num2 != '') {
+      calc();
+   }
 }
 
 function take_num (button) {
@@ -32,15 +58,23 @@ function calc() {
       switch (operator) {
          case "+":
             res.textContent =Number(num2) + Number(num1);
+            operator = '';
+            res_text = res.textContent;
             break;
          case "-":
             res.textContent =Number(num1) - Number(num2);
+            operator = '';
+            res_text = res.textContent;
             break;
          case "*":
             res.textContent =Number(num2) * Number(num1);
+            operator = '';
+            res_text = res.textContent;
             break;
          case "/":
             res.textContent =Number(num1) / Number(num2);
+            operator = '';
+            res_text = res.textContent;
             break;
          default:
             break;
